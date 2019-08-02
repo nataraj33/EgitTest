7aca828612e6ff15fc4e3114131139a7c089d4d2
Natarajan <nataraj33@gmail.com>
2019-08-02 17:18:47 +0200

    Egit Test from eclipse first commit

---
+++ 1ServletHelloWorld/src/com/natsoft/TestServlet.java
@@ -0,0 +1,50 @@
+package com.natsoft;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet(description = "Test Hello World Servlet", urlPatterns = { "/TestServlet" })
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final String CONTENT_TYPE = 
+	        "text/html;charset=UTF-8";
+	      private static final String MESSAGE = "<!DOCTYPE html><html>" +
+	              "<head><title>Hello Natz!</title></head>" +
+	              "<body>Hello World WildFly</body>" +
+	              "</html>";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	    response.setContentType(CONTENT_TYPE);       
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println(MESSAGE);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
